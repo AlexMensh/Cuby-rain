29533324b6c0795502e5199af5ea21acca9e2690
AlexMensh <dobryajka1991@gmail.com>
2024-04-26 22:56:54 +0300

    upd: Spawner

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour
 {
-    [SerializeField] private float spawnRadius = 5f;
+    [SerializeField] private float _spawnRadius = 5f;
     [SerializeField] private Cube _cubePrefab;
 
     public void SpawnCube()
     {
-        Vector3 spawnPosition = transform.position + Random.insideUnitSphere * spawnRadius;
+        Vector3 spawnPosition = transform.position + Random.insideUnitSphere * _spawnRadius;
 
         Instantiate(_cubePrefab, spawnPosition, Quaternion.identity);
     }
